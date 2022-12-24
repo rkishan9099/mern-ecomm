f28f1feba111fffd8a3f4cf3a4e68d30717db49f
rkishan9099 <rkishan9099@gmail.com>
2022-12-24 09:49:49 +0530

    add first get route

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,5 +4,7 @@ import DBconnect from './config/dbConnect.js'
 dotenv.config();
 const app = express();
 DBconnect();
-
+app.get('/',(req,res,next)=>{
+  res.send("Hello World from Rkishan😊😁")
+})
 export default app;
