db2f77fd525ca471ba03ccc6ed1ca3fd5414ee70
rkishan9099 <rkishan9099@gmail.com>
2022-12-25 12:20:18 +0530

    Add Errorhandler Middleware for 404 error

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,10 +1,20 @@
 import express from 'express'
 import dotenv  from 'dotenv';
-import DBconnect from './config/dbConnect.js'
 dotenv.config();
+import db_connect from'./config/dbConnect.js'
+import ErrorHandler from './Middleware/errorHandler.js'
+//console.log(process.env.PORT)
 const app = express();
-DBconnect();
+app.use(express.json());
+db_connect();
 app.get('/',(req,res,next)=>{
   res.send("Hello World from Rkishan😊😁")
+  next('55555');
 })
+app.use((req,res,next)=>{
+  const err = new Error("Not Found");
+  err.status=404;
+  next(err);
+})
+app.use(ErrorHandler);
 export default app;
