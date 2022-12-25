bf64cd8056722e65272bdbcf4caec47c21dae0f5
rkishan9099 <rkishan9099@gmail.com>
2022-12-25 12:19:23 +0530

    remove cluster funtionality gorm tempory

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,5 @@
 import app from './src/app.js'
-import cluster from "cluster"
-import os from 'os'
-const totalCPUs = os.cpus().length;
-const PORT = process.env.PORT || 4000;
-if (cluster.isMaster) {
-  // Fork workers.
-  for (let i = 0; i < totalCPUs; i++) {
-    cluster.fork();
-  }
-  cluster.on("exit", (worker, code, signal) => {
-    cluster.fork();
-  });
-} else {
-  
-}
+const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`App listening on port ${PORT}`);
   });
